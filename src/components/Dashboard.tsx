52ea1f5587e5f7c8d9cef1450cc64d4f57a2a1ac
bucnariMihaela <bucnarimihaela0gmail.com>
2023-11-15 17:50:46 +0200

    first navbar commit

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 
 const Dashboard: React.FC = () => {
     return (
-        <h1> This is a Dashboard</h1>
+        <><h1> This is a Dashboard</h1>
+
+        </>
+
 );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,6 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route path="dashboard" element={<Dashboard/>} />
-
                 </Route>
             </Routes>
         </BrowserRouter>
